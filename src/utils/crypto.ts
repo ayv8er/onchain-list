f782cb48bcebcaa1f39dcab285ece65905680198
Richard Oh <richard@magic.link>
2024-12-24 01:51:42 -0500

    removes crypto vars during server build

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,18 +1,23 @@
+"use client";
 import CryptoJS from "crypto-js";
 
 const SECRET = process.env.NEXT_PUBLIC_SHARED_SECRET;
 const FIXED_SALT = process.env.NEXT_PUBLIC_FIXED_SALT;
 
-if (!SECRET) throw new Error("SECRET is not set");
-if (!FIXED_SALT) throw new Error("FIXED_SALT is not set");
+let key: CryptoJS.lib.WordArray | undefined;;
 
-const key = CryptoJS.PBKDF2(SECRET, FIXED_SALT, {
-  keySize: 256/32,
-  iterations: 1000
-});
+if (typeof window !== "undefined") {
+  if (!SECRET) throw new Error("SECRET is not set");
+  if (!FIXED_SALT) throw new Error("FIXED_SALT is not set");
+
+  key = CryptoJS.PBKDF2(SECRET, FIXED_SALT, {
+    keySize: 256/32,
+    iterations: 1000
+  });
+}
 
 export function encrypt(text: string) {
-  if (!text) return '';
+  if (!text || !key) return "";
   return CryptoJS.AES.encrypt(text, key, {
     mode: CryptoJS.mode.ECB,
     padding: CryptoJS.pad.Pkcs7
@@ -20,7 +25,7 @@ export function encrypt(text: string) {
 };
 
 export function decrypt(text: string) {
-  if (!text) return '';
+  if (!text || !key) return "";
   return CryptoJS.AES.decrypt(text, key, {
     mode: CryptoJS.mode.ECB,
     padding: CryptoJS.pad.Pkcs7
